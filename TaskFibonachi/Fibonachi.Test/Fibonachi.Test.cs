4c953d7150d8f86eb21d0fffad2b4dc7c8e50faf
Stas Strezh <37235564+StasStrezh@users.noreply.github.com>
2018-04-12 17:58:31 +0300

    Add files via upload

---
+++ TaskFibonachi/Fibonachi.Test/Fibonachi.Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TaskFibonachi;
+
+namespace TaskFibonachi
+{
+    [TestFixture]
+    public class Class1
+    {
+        [TestCase(2, new int[] { 0, 1, 1 })]
+        [TestCase(4, new int[] { 0, 1, 1, 2, 3 })]
+        [TestCase(6, new int[] { 0, 1, 1, 2, 3, 5, 8 })]
+        [TestCase(8, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21 })]
+        [TestCase(10, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 })]
+        public void Fibonachi_Tests(int num, int[] expRes)
+        {
+            int[] Fib = Fibonachi.Generator(num);
+            Assert.AreEqual(expRes, Fib);
+        }
+
+    }
+}
---
+++ TaskFibonachi/TaskFibonachi/Fibonachi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TaskFibonachi
+{
+    public class Fibonachi
+    {
+        public static int[] Generator(int num)
+        {
+            int[] array = new int[2];
+            array[0] = 0;
+            array[1] = 1;
+            int i = 1;
+            while(i<num+1)
+            {
+                Array.Resize<int>(ref array, i + 2);
+                array[i + 1] = Summ(array[i - 1], array[i]);
+                i++;
+                if (array[i] >= int.MaxValue)
+                    break;
+            }
+            Array.Resize<int>(ref array, i);
+            return array;
+        }
+        private static int Summ(int array1, int array2) => array1 + array2;
+    }
+}
